c57397d280e67cade33973a4868522fb0047d2b1
Garth Lilly <garff247@gmail.com>
2019-04-09 08:34:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,16 @@ def countdown(number)
   x = 0
   while number > x
     puts "#{number} SECOND(S)!"
+    number  -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+  x = 0
+  while number > x
+    puts "#{number} SECOND(S)!"
     sleep(1)
     number  -= 1
   end
